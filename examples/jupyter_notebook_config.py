56ecc9f5eeab128de217a56a7a61b324436e313e
Mohammad Mohammadi <mohammad@exabyte.io>
2018-11-02 12:00:48 -0700

    Use realpath to convert notebooks to scripts

--- examples/jupyter_notebook_config.py
+++ examples/jupyter_notebook_config.py
@@ -1,8 +1,10 @@
 import io
 import os
+
 from notebook.utils import to_api_path
 
 _script_exporter = None
+
 
 def script_post_save(model, os_path, contents_manager, **kwargs):
     """convert notebooks to Python script after save with nbconvert
@@ -21,12 +23,13 @@ def script_post_save(model, os_path, contents_manager, **kwargs):
 
     log = contents_manager.log
 
-    base, ext = os.path.splitext(os_path)
+    base, ext = os.path.splitext(os.path.realpath(os_path))
     script, resources = _script_exporter.from_filename(os_path)
     script_fname = base + resources.get('output_extension', '.txt')
     log.info("Saving script /%s", to_api_path(script_fname, contents_manager.root_dir))
 
     with io.open(script_fname, 'w', encoding='utf-8') as f:
         f.write(script)
+
 
 c.FileContentsManager.post_save_hook = script_post_save

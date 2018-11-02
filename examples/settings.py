b265b8fc04d1d5c267bb3f943fdfb5e6c1f26c75
Mohammad Mohammadi <mohammad@exabyte.io>
2018-11-01 20:31:58 -0700

    created settings script

---
+++ examples/settings.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Overview
+# 
+# This file contains settings that are used inside the examples. Settings are imported inside the examples via [nbimporter](https://github.com/grst/nbimporter) package.
+# 
+# # Settings
+# 
+# - **HOST**: Hostname of the RESTful API server. Defaults to platform.exabyte.io.
+# 
+# 
+# - **PORT**: The port RESTful API server is listening on. Defaults to 443.
+# 
+# 
+# - **VERSION**: RESTFul API version. Defaults to 2018-10-01.
+# 
+# 
+# - **SECURE**: Whether to use secure connection. Defaults to True.
+# 
+# 
+# - **ACCOUNT_ID**: Your account ID.
+# 
+# 
+# - **AUTH_TOKEN**: Your authentication token.
+
+# In[ ]:
+
+
+PORT = 443
+SECURE = True
+VERSION = "2018-10-01"
+HOST = "platform.exabyte.io"
+
+ACCOUNT_ID = "knJyjbqxww7kt4GpA"
+AUTH_TOKEN = "Jh6umOZqjfTQldC4gMnObb-rdoiNMg87ga0yZ-wcnQ9"
+

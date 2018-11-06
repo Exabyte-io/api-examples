e74eedcf12491ae3a7dfbfb54b676465e83e99f9
Mohammad Mohammadi <mohammad@exabyte.io>
2018-11-06 15:44:16 -0800

    Updated utils

--- examples/utils.py
+++ examples/utils.py
@@ -13,15 +13,27 @@ from tabulate import tabulate
 def get_jobs_statuses_by_ids(endpoint, job_ids):
     """
     Get jobs statues by their IDs.
+    
+    Args:
+        endpoint (endpoints.jobs.JobEndpoints): an instance of job endpoints class
+        job_ids (list): list of job IDs to get the status for
+    
+    Returns:
+        list: list of job statuses
     """
     jobs = endpoint.list({"_id": {"$in": job_ids}}, {"fields": {"status": 1}})
     return [job["status"] for job in jobs]
 
 
-def wait_for_jobs_to_finish(endpoint, job_ids, job_pull_interval=60):
+def wait_for_jobs_to_finish(endpoint, job_ids, pulling_interval=60):
     """
-    Wait for jobs to finish. 
+    Wait for jobs to finish and print their statuses.
     A job is considered finished if it is not in "pre-submission", "submitted", or, "active" status.
+    
+    Args:
+        endpoint (endpoints.jobs.JobEndpoints): an instance of job endpoints class
+        job_ids (list): list of job IDs to wait for
+        pulling_interval (int): job pulling interval in seconds. Defaults to 60.
     """
     while True:
         statuses = get_jobs_statuses_by_ids(endpoint, job_ids)
@@ -36,5 +48,5 @@ def wait_for_jobs_to_finish(endpoint, job_ids, job_pull_interval=60):
         print tabulate([row], headers, tablefmt='grid', stralign='center')
 
         if all([status not in ["pre-submission", "submitted", "active"] for status in statuses]): break
-        time.sleep(job_pull_interval)
+        time.sleep(pulling_interval)
 

13af4676e912bc2f6b1abbd535cc5df349cfe2de
Brendan Smith <brendan@exabyte.io>
2021-06-30 13:27:47 -0400

    Fixed bug related to getting the notebook environment - downstream, this caused jupyter packages to be installed in colab still

--- examples/utils/executable_for_notebook_runtimes.py
+++ examples/utils/executable_for_notebook_runtimes.py
@@ -3,23 +3,27 @@ environment_variables_config = {'ACCOUNT_ID': ACCOUNT_ID,
                                 'MATERIALS_PROJECT_API_KEY': MATERIALS_PROJECT_API_KEY,
                                 'ORGANIZATION_ID': ORGANIZATION_ID}
 
-def set_notebook_environment(notebook_environment):
+def set_notebook_environment(environment_variables_config):
     """
     This function sets the notebook environment by calling to install the needed packages
     and setting the variables in settings.json (if needed)
     Args:
-        notebook_environment (str): the environment of our notebook.
-        Ex) "Jupyter", "Colab", etc.
+        environment_variables_config (dict): contains key value pairs needed to set up a
+            certain notebook (or frontend) runtime.
+            Ex) environment_variables_config['ACCOUNT_ID'] = ACCOUNT_ID
+                environment_variables_config[notebook_environment] = "Jupyter"
+
     Return:
         None
     """
-
+    notebook_environment = environment_variables_config['notebook_environment']
     if notebook_environment == 'Colab':
         from utils.colab import setup_colab_runtime_environment
         setup_colab_runtime_environment(environment_variables_config)
     else:
         from utils.generic import ensure_packages_are_installed
-        ensure_packages_are_installed(environment_variables_config)
+        ensure_packages_are_installed(notebook_environment)
+
 
 def execute():
     """
@@ -38,15 +42,13 @@ def execute():
         notebook_name = _message.blocking_request('get_ipynb')['ipynb']['metadata']['colab']['name']
         notebook_path = glob.glob('**/'+notebook_name, recursive=True)[0][0:-len(notebook_name)]
         os.chdir(notebook_path) # go to the folder in the repo where one would be if this was in local Jupyter
-
     elif 'ZMQInteractiveShell' in str(get_ipython()):
         environment_variables_config.update({'notebook_environment': 'Jupyter'})
-
     else:
         environment_variables_config.update({'notebook_environment': ''})
 
     module_path = os.path.abspath(os.path.join('..'))
     if module_path not in sys.path: sys.path.append(module_path)
-    set_notebook_environment(environment_variables_config['notebook_environment'])
+    set_notebook_environment(environment_variables_config)
 
 execute()

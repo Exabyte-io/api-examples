72b18e90344bdd4bb43268f96184b0eabb3279f0
Brendan Smith <brendan@exabyte.io>
2021-07-07 08:20:11 -0400

    Fixing the url that we execute to setup the notebook environment. NOTE: the word blob that appears on github website urls is not present when viewing the page as raw.

--- examples/material/create_material.py
+++ examples/material/create_material.py
@@ -29,7 +29,7 @@ ORGANIZATION_ID  = "ORGANIZATION_ID" #@param {type:"string"}
 import os, glob, sys, importlib, urllib.request
 
 # The below execution sets up runtime using code stored remotely in a url
-exec(urllib.request.urlopen('https://raw.githubusercontent.com/Exabyte-io/exabyte-api-examples/blob/dev/examples/utils/initialize_settings.py').read())
+exec(urllib.request.urlopen('https://raw.githubusercontent.com/Exabyte-io/exabyte-api-examples/dev/examples/utils/initialize_settings.py').read())
 
 
 # # Imports

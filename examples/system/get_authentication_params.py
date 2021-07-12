1ffcdd81e574c20f530dc51021b969d52fcaebd5
Brendan Smith <brendan@exabyte.io>
2021-07-12 15:46:44 -0400

    Starting to convert the system/get_authentication_params notebook to be usable with Colab

--- examples/system/get_authentication_params.py
+++ examples/system/get_authentication_params.py
@@ -1,29 +1,33 @@
 #!/usr/bin/env python
 # coding: utf-8
 
+# <a href="https://colab.research.google.com/github/Exabyte-io/exabyte-api-examples/blob/feature/SOF-4618/examples/system/get_authentication_params.ipynb" target="_blank">Open in Google Colab</a>
+
 # # Overview
 # 
 # This example shows how to log in to Exabyte RESTFul API via [Login](https://docs.exabyte.io/api/API/post_login) endpoint and generate API authentication parameters.
+# 
+# Here, we execute a remote URL to set our notebook environment. Do not edit the following cell's contents.
+
+
+
+ACCOUNT_ID = AUTH_TOKEN = MATERIALS_PROJECT_API_KEY = ORGANIZATION_ID = ''
+import os, glob, sys, importlib, urllib.request
+
+# The below execution sets up runtime using code stored remotely in a url
+exec(urllib.request.urlopen('https://raw.githubusercontent.com/Exabyte-io/exabyte-api-examples/dev/examples/utils/initialize_settings.py').read())
+
 
 # # Execution
 # 
 # > <span style="color: orange">**NOTE**</span>: In order to run this example, an active Exabyte.io account is required.
-# 
+
 # ## Import packages
 
-# In[]:
 
 
-import os
-import sys
-
-
-# Import settings and utils file
-module_path = os.path.abspath(os.path.join('..'))
-if module_path not in sys.path: sys.path.append(module_path)
 from settings import HOST, PORT, VERSION, SECURE
-from utils.generic import ensure_packages_are_installed, display_JSON
-ensure_packages_are_installed()
+from utils.generic import display_JSON
 
 from exabyte_api_client.endpoints.login import LoginEndpoint
 
@@ -34,7 +38,6 @@ from exabyte_api_client.endpoints.login import LoginEndpoint
 # 
 # - **PASSWORD**: Your Exabyte account password.
 
-# In[]:
 
 
 USERNAME = "YOUR_USERNANE"
@@ -43,7 +46,6 @@ PASSWORD = "YOUR_PASSWORD"
 
 # ## Initialize the endpoint
 
-# In[]:
 
 
 endpoint = LoginEndpoint(HOST, PORT, USERNAME, PASSWORD, VERSION, SECURE)
@@ -54,8 +56,6 @@ auth_params = endpoint.login()
 # 
 # Print the authentication parameters in pretty JSON below. Update [settings](../settings.py) with this parameters to be able to run other examples.
 
-# In[]:
 
 
 display_JSON(auth_params)
-

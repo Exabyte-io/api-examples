05a0be218fcaabc6742a05c95d37d280393762dd
Brendan Smith <brendan@exabyte.io>
2021-08-09 13:54:01 -0700

    Test Colab button

--- examples/material/api_interoperability_showcase.py
+++ examples/material/api_interoperability_showcase.py
@@ -1,7 +1,11 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# <a href="https://colab.research.google.com/github/Exabyte-io/exabyte-api-examples/blob/feature/SOF-4685/examples/material/api_interoperability_showcase.ipynb" target="_blank">Open in Google Colab</a>
+# <button>
+# <a href="https://colab.research.google.com/github/Exabyte-io/exabyte-api-examples/blob/feature/SOF-4685/examples/material/api_interoperability_showcase.ipynb" target="_blank">
+# <img alt="Open in Google Colab" src="https://algotrading101.com/learn/wp-content/uploads/2021/05/Google-Colab-Guide-1024x683.jpg" width="150" border="0">
+# </a>
+# </button>
 
 # # Overview
 # 

509034d476378dfb37cde02b9ac630b86ab783cf
Brendan Smith <brendan@exabyte.io>
2021-08-11 13:47:18 -0400

    updating initialize_settings.py with is_setup_executed variable

--- examples/utils/initialize_settings.py
+++ examples/utils/initialize_settings.py
@@ -5,6 +5,7 @@
 #
 # If using Jupyter, these variables can be left to their default values in the code cell, but the user
 # should change these values in the settings.json file located in the examples folder.
+import os
 
 environment_variables_config = {'ACCOUNT_ID': ACCOUNT_ID,
                                 'AUTH_TOKEN': AUTH_TOKEN,
@@ -43,20 +44,22 @@ def execute():
         None
     """
 
-    if 'google.colab' in str(get_ipython()):
-        environment_variables_config.update({'notebook_environment': 'Colab'})
-        get_ipython().system('git clone https://github.com/Exabyte-io/exabyte-api-examples.git')
-        from google.colab import _message
-        notebook_name = _message.blocking_request('get_ipynb')['ipynb']['metadata']['colab']['name']
-        notebook_path = glob.glob('**/'+notebook_name, recursive=True)[0][0:-len(notebook_name)]
-        os.chdir(notebook_path) # go to the folder in the repo where one would be if this was in local Jupyter
-    elif 'ZMQInteractiveShell' in str(get_ipython()):
-        environment_variables_config.update({'notebook_environment': 'Jupyter'})
-    else:
-        environment_variables_config.update({'notebook_environment': ''})
+    if 'is_setup_executed' not in os.environ:
+        if 'google.colab' in str(get_ipython()):
+            environment_variables_config.update({'notebook_environment': 'Colab'})
+            get_ipython().system('git clone https://github.com/Exabyte-io/exabyte-api-examples.git')
+            from google.colab import _message
+            notebook_name = _message.blocking_request('get_ipynb')['ipynb']['metadata']['colab']['name']
+            notebook_path = glob.glob('**/'+notebook_name, recursive=True)[0][0:-len(notebook_name)]
+            os.chdir(notebook_path) # go to the folder in the repo where one would be if this was in local Jupyter
+        elif 'ZMQInteractiveShell' in str(get_ipython()):
+            environment_variables_config.update({'notebook_environment': 'Jupyter'})
+        else:
+            environment_variables_config.update({'notebook_environment': ''})
 
-    module_path = os.path.abspath(os.path.join('..'))
-    if module_path not in sys.path: sys.path.append(module_path)
-    set_notebook_environment(environment_variables_config)
+        module_path = os.path.abspath(os.path.join('..'))
+        if module_path not in sys.path: sys.path.append(module_path)
+        set_notebook_environment(environment_variables_config)
 
 execute()
+os.environ.update({'is_setup_executed': 'True'})

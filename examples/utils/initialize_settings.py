b656f13a88d76864f9a3f483a41d376c15b9e8d2
Max Rakitin <max.rakitin@exabyte.io>
2023-06-08 01:01:35 -0400

    REF: get currently running notebook in Colab via querying an internal URL; some style fixes with flake8/isort

--- examples/utils/initialize_settings.py
+++ examples/utils/initialize_settings.py
@@ -6,10 +6,19 @@
 # If using Jupyter, these variables can be left to their default values in the code cell, but the user
 # should change these values in the settings.json file located in the examples folder.
 
-environment_variables_config = {'ACCOUNT_ID': ACCOUNT_ID,
-                                'AUTH_TOKEN': AUTH_TOKEN,
-                                'MATERIALS_PROJECT_API_KEY': MATERIALS_PROJECT_API_KEY,
-                                'ORGANIZATION_ID': ORGANIZATION_ID}
+import glob
+import os
+import socket
+import sys
+
+import requests
+from IPython import get_ipython
+
+environment_variables_config = {'ACCOUNT_ID': ACCOUNT_ID,  # noqa F821
+                                'AUTH_TOKEN': AUTH_TOKEN,  # noqa F821
+                                'MATERIALS_PROJECT_API_KEY': MATERIALS_PROJECT_API_KEY,  # noqa F821
+                                'ORGANIZATION_ID': ORGANIZATION_ID}  # noqa F821
+
 
 def set_notebook_environment(environment_variables_config):
     """
@@ -33,6 +42,19 @@ def set_notebook_environment(environment_variables_config):
         ensure_packages_are_installed(notebook_environment)
 
 
+def get_notebook_name():
+    """
+    Get the name of a currently running notebook in Google Colab.
+    Args:
+        None
+    Return:
+        filename
+    """
+    ip = socket.gethostbyname(socket.gethostname())  # 172.28.0.12
+    filename = requests.get(f"http://{ip}:9000/api/sessions").json()[0]["name"]
+    return filename
+
+
 def execute():
     """
     Main execution function. This function determines and sets the runtime environment
@@ -44,21 +66,24 @@ def execute():
     """
 
     if 'is_setup_executed' not in os.environ:
-        if 'google.colab' in str(get_ipython()):
+        ip = get_ipython()
+        if 'google.colab' in str(ip):
+            branch = os.getenv("GIT_BRANCH", "dev")  # a way to inject a branch of interest from Colab if run via a PR.
             environment_variables_config.update({'notebook_environment': 'Colab'})
-            get_ipython().system('git clone https://github.com/Exabyte-io/exabyte-api-examples.git')
-            from google.colab import _message
-            notebook_name = _message.blocking_request('get_ipynb')['ipynb']['metadata']['colab']['name']
-            notebook_path = glob.glob('**/'+notebook_name, recursive=True)[0][0:-len(notebook_name)]
-            os.chdir(notebook_path) # go to the folder in the repo where one would be if this was in local Jupyter
-        elif 'ZMQInteractiveShell' in str(get_ipython()):
+            ip.system(f"git clone --single-branch -b {branch} https://github.com/Exabyte-io/exabyte-api-examples.git")
+            notebook_name = get_notebook_name()
+            notebook_path = glob.glob(f"**/{notebook_name}", recursive=True)[0][0:-len(notebook_name)]
+            os.chdir(notebook_path)  # go to the folder in the repo where one would be if this was in local Jupyter
+        elif 'ZMQInteractiveShell' in str(ip):
             environment_variables_config.update({'notebook_environment': 'Jupyter'})
         else:
             environment_variables_config.update({'notebook_environment': ''})
 
         module_path = os.path.abspath(os.path.join('..'))
-        if module_path not in sys.path: sys.path.append(module_path)
+        if module_path not in sys.path:
+            sys.path.append(module_path)
         set_notebook_environment(environment_variables_config)
+
 
 execute()
 os.environ.update({'is_setup_executed': 'True'})

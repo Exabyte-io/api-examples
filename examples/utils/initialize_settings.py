920bf484cca3007abd8d3c21a5f666301a92ef1a
Max Rakitin <max.rakitin@exabyte.io>
2023-06-08 01:06:27 -0400

    STY: apply formatting with `black`

--- examples/utils/initialize_settings.py
+++ examples/utils/initialize_settings.py
@@ -14,10 +14,12 @@ import sys
 import requests
 from IPython import get_ipython
 
-environment_variables_config = {'ACCOUNT_ID': ACCOUNT_ID,  # noqa F821
-                                'AUTH_TOKEN': AUTH_TOKEN,  # noqa F821
-                                'MATERIALS_PROJECT_API_KEY': MATERIALS_PROJECT_API_KEY,  # noqa F821
-                                'ORGANIZATION_ID': ORGANIZATION_ID}  # noqa F821
+environment_variables_config = {
+    "ACCOUNT_ID": ACCOUNT_ID,  # noqa F821
+    "AUTH_TOKEN": AUTH_TOKEN,  # noqa F821
+    "MATERIALS_PROJECT_API_KEY": MATERIALS_PROJECT_API_KEY,  # noqa F821
+    "ORGANIZATION_ID": ORGANIZATION_ID,  # noqa F821
+}
 
 
 def set_notebook_environment(environment_variables_config):
@@ -33,12 +35,14 @@ def set_notebook_environment(environment_variables_config):
     Return:
         None
     """
-    notebook_environment = environment_variables_config['notebook_environment']
-    if notebook_environment == 'Colab':
+    notebook_environment = environment_variables_config["notebook_environment"]
+    if notebook_environment == "Colab":
         from utils.colab import setup_colab_runtime_environment
+
         setup_colab_runtime_environment(environment_variables_config)
     else:
         from utils.generic import ensure_packages_are_installed
+
         ensure_packages_are_installed(notebook_environment)
 
 
@@ -65,25 +69,25 @@ def execute():
         None
     """
 
-    if 'is_setup_executed' not in os.environ:
+    if "is_setup_executed" not in os.environ:
         ip = get_ipython()
-        if 'google.colab' in str(ip):
+        if "google.colab" in str(ip):
             branch = os.getenv("GIT_BRANCH", "dev")  # a way to inject a branch of interest from Colab if run via a PR.
-            environment_variables_config.update({'notebook_environment': 'Colab'})
+            environment_variables_config.update({"notebook_environment": "Colab"})
             ip.system(f"git clone --single-branch -b {branch} https://github.com/Exabyte-io/exabyte-api-examples.git")
             notebook_name = get_notebook_name()
-            notebook_path = glob.glob(f"**/{notebook_name}", recursive=True)[0][0:-len(notebook_name)]
+            notebook_path = glob.glob(f"**/{notebook_name}", recursive=True)[0][0 : -len(notebook_name)]
             os.chdir(notebook_path)  # go to the folder in the repo where one would be if this was in local Jupyter
-        elif 'ZMQInteractiveShell' in str(ip):
-            environment_variables_config.update({'notebook_environment': 'Jupyter'})
+        elif "ZMQInteractiveShell" in str(ip):
+            environment_variables_config.update({"notebook_environment": "Jupyter"})
         else:
-            environment_variables_config.update({'notebook_environment': ''})
+            environment_variables_config.update({"notebook_environment": ""})
 
-        module_path = os.path.abspath(os.path.join('..'))
+        module_path = os.path.abspath(os.path.join(".."))
         if module_path not in sys.path:
             sys.path.append(module_path)
         set_notebook_environment(environment_variables_config)
 
 
 execute()
-os.environ.update({'is_setup_executed': 'True'})
+os.environ.update({"is_setup_executed": "True"})

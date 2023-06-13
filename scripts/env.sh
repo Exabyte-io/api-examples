2e5ca3c431be5e055349a01edbd2ec1bf19cf603
Max Rakitin <max.rakitin@exabyte.io>
2023-06-13 03:18:20 -0400

    Update the auth notebook to use a single script to set up the env

--- scripts/env.sh
+++ scripts/env.sh
@@ -10,6 +10,7 @@ GIT_BRANCH="${GIT_BRANCH:-'dev'}"
 git clone https://github.com/Exabyte-io/api-examples.git --single-branch --branch ${GIT_BRANCH}
 
 python -m pip install -r api-examples/requirements-colab.txt
-python -m pip install ./api-examples/utils/
+python -m pip install ./api-examples/examples/
 
+# TODO: consider removing it for Colab?
 # python api-examples/examples/utils/initialize_settings.py  # the script with cd into the current notebook dir

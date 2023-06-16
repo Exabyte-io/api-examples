de247b7ca1dcbedf9552c96e2401a98f063d3fec
Max Rakitin <max.rakitin@exabyte.io>
2023-06-16 04:39:28 -0400

    MNT: absolute import of settings

--- examples/utils/generic.py
+++ examples/utils/generic.py
@@ -3,7 +3,7 @@ import time
 import datetime
 import os
 import importlib.util
-from .. import settings
+from examples import settings
 import urllib
 from IPython.display import display, JSON
 import json

0179594fd0d5e5f109e38b46cf0d65444f9d21a3
Max Rakitin <max.rakitin@exabyte.io>
2023-06-19 16:03:20 -0400

    FIX: update references from Exabyte/.io -> Mat3ra/.com per PR review
    - ml-train-model-predict-properties.*

--- examples/job/ml-train-model-predict-properties.py
+++ examples/job/ml-train-model-predict-properties.py
@@ -7,7 +7,7 @@
 
 # # Overview
 # 
-# This example demonstrates how to use Exabyte RESTful API to build a machine learning (ML) model for a set of materials called "train materials" and use the model to predict properties of another set called "target materials". The general approach can work for multiple properties, we use the Electronic Band Gap in this example.
+# This example demonstrates how to use Mat3ra RESTful API to build a machine learning (ML) model for a set of materials called "train materials" and use the model to predict properties of another set called "target materials". The general approach can work for multiple properties, we use the Electronic Band Gap in this example.
 # 
 # 
 # 
@@ -24,12 +24,12 @@
 # 
 # ## Pre-requisites
 # 
-# The explanation below assumes that the reader is familiar with the concepts used in Exabyte platform and RESTful API. We outline these below and direct the reader to the original sources of information:
+# The explanation below assumes that the reader is familiar with the concepts used in Mat3ra platform and RESTful API. We outline these below and direct the reader to the original sources of information:
 # 
 # - [Generating RESTful API authentication parameters](../system/get_authentication_params.ipynb)
 # - [Importing materials from materials project](../material/import_materials_from_materialsproject.ipynb)
 # - [Creating and submitting jobs](./create_and_submit_job.ipynb)
-# - [Running DFT calculations](./dft-band-gaps-calculation.ipynb)
+# - [Running DFT calculations](./run-simulations-and-extract-properties.ipynb)
 
 # # Complete Authorization Form and Initialize Settings
 # 
@@ -37,7 +37,7 @@
 # 
 # If you are running this notebook from Google Colab, Colab takes ~1 min to execute the following cell.
 # 
-# ACCOUNT_ID and AUTH_TOKEN - Authentication parameters needed for when making requests to [Exabyte.io's API Endpoints](https://docs.mat3ra.com/rest-api/endpoints/).
+# ACCOUNT_ID and AUTH_TOKEN - Authentication parameters needed for when making requests to [Mat3ra.com's API Endpoints](https://docs.mat3ra.com/rest-api/endpoints/).
 # 
 # MATERIALS_PROJECT_API_KEY - Authentication parameter needed for when making requests to [Material Project's API](https://materialsproject.org/open)
 # 

35a56e9589b10fd83e29a770dd1d0eae0905c0bd
VsevolodX <79542055+VsevolodX@users.noreply.github.com>
2023-11-29 14:25:36 -0800

    chore: fix versions and simplify

--- other/python_transformations/imports.py
+++ other/python_transformations/imports.py
@@ -1,20 +1,19 @@
 """Installation of all supported packages"""
 import micropip
 
-await micropip.install("https://files.mat3ra.com:44318/uploads/pymatgen-2023.9.10-py3-none-any.whl", deps=False)
-print("Installed pymatgen")
-await micropip.install("https://files.mat3ra.com:44318/web/pyodide/spglib-2.0.2-py3-none-any.whl", deps=False)
-print("Installed spglib")
-await micropip.install(
+for pkg in [
+    "https://files.mat3ra.com:44318/uploads/pymatgen-2023.9.10-py3-none-any.whl",
+    "https://files.mat3ra.com:44318/web/pyodide/spglib-2.0.2-py3-none-any.whl",
     "https://files.pythonhosted.org/packages/d9/0e/2a05efa11ea33513fbdf4a2e2576fe94fd8fa5ad226dbb9c660886390974/ruamel.yaml-0.17.32-py3-none-any.whl",
-    deps=False,
-)
-print("Installed ruamel.yaml")
+]:
+    await micropip.install(pkg, deps=False)
+    print("Installed " + pkg)
+
 for pkg in [
     "ase==3.22.1",
-    "networkx==3.2.1",
+    "networkx==3.1",
     "monty==2023.11.3",
-    "scipy==1.11.4",
+    "scipy==1.11.1",
     "lzma",
     "tabulate==0.9.0",
     "sqlite3",

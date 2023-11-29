fdb5cf1443969a830e2bd2593228d5a3c4674ee8
VsevolodX <79542055+VsevolodX@users.noreply.github.com>
2023-11-29 14:20:21 -0800

    update: add versions to packages if they are known

--- other/python_transformations/imports.py
+++ other/python_transformations/imports.py
@@ -2,10 +2,23 @@
 import micropip
 
 await micropip.install("https://files.mat3ra.com:44318/uploads/pymatgen-2023.9.10-py3-none-any.whl", deps=False)
+print("Installed pymatgen")
 await micropip.install("https://files.mat3ra.com:44318/web/pyodide/spglib-2.0.2-py3-none-any.whl", deps=False)
+print("Installed spglib")
 await micropip.install(
     "https://files.pythonhosted.org/packages/d9/0e/2a05efa11ea33513fbdf4a2e2576fe94fd8fa5ad226dbb9c660886390974/ruamel.yaml-0.17.32-py3-none-any.whl",
     deps=False,
 )
-for pkg in ["ase", "networkx", "monty", "scipy", "lzma", "tabulate", "sqlite3", "sympy"]:
+print("Installed ruamel.yaml")
+for pkg in [
+    "ase==3.22.1",
+    "networkx==3.2.1",
+    "monty==2023.11.3",
+    "scipy==1.11.4",
+    "lzma",
+    "tabulate==0.9.0",
+    "sqlite3",
+    "sympy==1.12",
+]:
     await micropip.install(pkg)
+    print("Installed " + pkg)

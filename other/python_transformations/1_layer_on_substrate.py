990139f1077fbdbb9042401e22253140d4b826a5
VsevolodX <79542055+VsevolodX@users.noreply.github.com>
2023-12-08 19:16:17 -0800

    chore: add comments and rename

--- other/python_transformations/1_layer_on_substrate.py
+++ other/python_transformations/1_layer_on_substrate.py
@@ -1,52 +1,62 @@
-"""Simple Materials Interface"""
+"""NAME: Layer on Substrate"""
 
-"""BLOCK: Packages Import"""
-# This block handles the import of necessary packages for the script.
-# Micropip is used to install packages in a Pyodide environment.
-# ASE (Atomic Simulation Environment) is installed as it is required for manipulating atoms, molecules and crystals.
+"""BLOCK: Install Dependencies"""
+# This block handles the installation of necessary packages for the script using Micropip in a Pyodide environment.
+# ASE (Atomic Simulation Environment) is installed for manipulating structures.
 import micropip
 
 await micropip.install("ase")
 print("installed ase")
 
-
 """BLOCK: Classes and Definitions"""
-# Define indices to identify the substrate and layer from the list of input materials.
-# These materials are expected to be provided as primitive cells of a 3D material (substrate)
-# and a 2D material (layer), which will form the interface.
+
+"""
+NOTE: edit the variables above for your specific use case.
+"""
+# Indices identifying the substrate and layer from the list of input materials.
 SUBSTRATE_INDEX = 0
 LAYER_INDEX = 1
 
-# Set Miller indices (h, k, l) for the resulting surface
-SURFACE_MILLER_H = 1
-SURFACE_MILLER_K = 1
-SURFACE_MILLER_L = 1
-SURFACE_VACUUM = 5  # defines the vacuum spacing (in Ångströms) added to the SURFACE unit cell.
-SURFACE_NUMBER_OF_LAYERS = 3
+SETTINGS = {
+    "surface": {
+        # Set Miller indices (h, k, l) for the resulting substrate surface
+        "miller:h": 1,
+        "miller:k": 1,
+        "miller:l": 1,
+        # The vacuum space (in Ångströms) added to the surface in the direction perpendicular to the surface
+        "vacuum": 5,
+        # The number of atomic layers in the resulting substrate
+        "number_of_layers": 3,
+    },
+    "interface": {
+        # The transformation matrix for the surface
+        "surface_v:matrix": [[1, 0], [0, 1]],
+        # The transformation matrix for the surface
+        "layer_v:matrix": [[1, 0], [0, 1]],
+        # Distance between the substrate and the layer (in Ångströms)
+        "distance": 2.0,
+    },
+}
 
-# Set parameters of the interface
-# Matrices of deformation for the surface and the layer
-INTERFACE_SURFACE_V_MATRIX = [[1, 0], [0, 1]]
-INTERFACE_LAYER_V_MATRIX = [[1, 0], [0, 1]]
-INTERFACE_DISTANCE = 2.0
 
+"""
+NOTE: DO NOT edit code below unless you know what you are doing.
+"""
 
-# DO NOT EDIT code below unless you are developing a new transformation.
-# The required imports
 from ase.build import surface as make_surface, supercells
 from ase.io import read, write
 import io
 import numpy as np
 
 
-# The following functions are used to convert between the POSCAR format and the ASE Atoms object.
-def ase_poscar_to_atoms(poscar):
+# The following 3 util functions are used for convenience purposes.
+def poscar_to_atoms(poscar):
     input = io.StringIO(poscar)
     atoms = read(input, format="vasp")
     return atoms
 
 
-def ase_atoms_to_poscar(atoms):
+def atoms_to_poscar(atoms):
     output = io.StringIO()
     write(output, atoms, format="vasp")
     content = output.getvalue()
@@ -60,12 +70,16 @@ def expand_matrix_2x2_to_3x3(matrix_2x2):
     return matrix_3x3
 
 
-# The `MaterialInterface` class encapsulates the creation and manipulation of a material interface.
-# It includes methods to create the structure of the interface, calculate strain and distance between layers.
 class MaterialInterface:
+    """
+    This class Encapsulates the creation and manipulation of a material interface.
+    Includes methods to create the structure of the interface, calculate strain and distance between layers.
+    """
+
     def __init__(self, substrate, material, settings=None):
         self.substrate = substrate
         self.material = material
+        self.original_material = self.material.copy()
         self.settings = settings
         if settings:
             for key in self.settings.keys():
@@ -74,6 +88,9 @@ class MaterialInterface:
         self.structure = self.create_structure()
 
     def create_structure(self):
+        """
+        Creates the interface structure from the substrate and the material.
+        """
         surface = self.settings["surface"]
         interface = self.settings["interface"]
 
@@ -90,7 +107,7 @@ class MaterialInterface:
         self.substrate = supercells.make_supercell(self.substrate, surface_v_matrix)
         self.substrate.wrap()
         self.material = supercells.make_supercell(self.material, layer_v_matrix)
-        self.original_material = self.material.copy()
+
         self.material.set_cell(self.substrate.get_cell(), scale_atoms=True)
         self.material.wrap()
 
@@ -101,8 +118,9 @@ class MaterialInterface:
         return interface
 
     def calculate_strain(self, substrate=None, material=None):
-        """Calculates strain for the material layer on the substrate"""
-
+        """
+        Calculates strain between the layer and the substrate.
+        """
         if substrate is None:
             substrate = self.substrate
         if material is None:
@@ -123,7 +141,9 @@ class MaterialInterface:
         return {"a": strain_a, "b": strain_b}
 
     def calculate_distance(self):
-        """Calculates distance between the substrate and the material"""
+        """
+        Calculates distance between the substrate and the material
+        """
         interface = self.settings["interface"]
         z_max_substrate = max(self.substrate.positions[:, 2])
         z_min_material = min(self.material.positions[:, 2])
@@ -132,34 +152,23 @@ class MaterialInterface:
         return z_offset
 
 
-# The function 'transform' will serve as the main execution point for the transformation.
-# It creates a `MaterialInterface` object with input materials and settings,
-# calculates the interface structure, and handles output.
-def transform():
-    settings = {
-        "surface": {
-            "miller:h": SURFACE_MILLER_H,
-            "miller:k": SURFACE_MILLER_K,
-            "miller:l": SURFACE_MILLER_L,
-            "vacuum": SURFACE_VACUUM,
-            "number_of_layers": SURFACE_NUMBER_OF_LAYERS,
-        },
-        "interface": {
-            "surface_v:matrix": INTERFACE_SURFACE_V_MATRIX,
-            "layer_v:matrix": INTERFACE_LAYER_V_MATRIX,
-            "distance": INTERFACE_DISTANCE,
-        },
-    }
+def main():
+    """
+    The main function of the script.
+    Creates a `MaterialInterface` object with input materials and settings.
+    Calculates the interface structure, and handles output.
 
-    # The input materials are provided as a list of dictionaries in ESSE Materials format.
+    Returns:
+        globals(): The globals() dictionary is returned to the platform JS environment.
+    """
     materials = globals()["materials_in"]
-    substrate_data = materials[SUBSTRATE_INDEX].getAsPOSCAR()
-    layer_data = materials[LAYER_INDEX].getAsPOSCAR()
+    substrate_data = materials[SUBSTRATE_INDEX]
+    layer_data = materials[LAYER_INDEX]
 
-    substrate = ase_poscar_to_atoms(substrate_data)
-    layer = ase_poscar_to_atoms(layer_data)
+    substrate = poscar_to_atoms(substrate_data)
+    layer = poscar_to_atoms(layer_data)
 
-    interface = MaterialInterface(substrate, layer, settings)
+    interface = MaterialInterface(substrate, layer, SETTINGS)
 
     print("Interface structure: ", interface.structure)
     print("Strain alongside lattice a:", interface.calculate_strain()["a"])
@@ -167,13 +176,11 @@ def transform():
 
     globals()["materials_out"] = [
         {
-            "poscar": ase_atoms_to_poscar(interface.structure),
+            "poscar": atoms_to_poscar(interface.structure),
         }
     ]
 
-    # Return the globals() dictionary to the platform JS environment.
     return globals()
 
 
-# Execute the main function to preform the material transformation process.
-transform()
+main()

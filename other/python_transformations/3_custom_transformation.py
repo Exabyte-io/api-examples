21067b6047c6e493269545fb0f74f70c80a996c7
Timur Bazhirov <timur@exabyte.io>
2023-12-08 22:13:50 -0800

    chore: minor cleanup

--- other/python_transformations/3_custom_transformation.py
+++ other/python_transformations/3_custom_transformation.py
@@ -21,10 +21,11 @@ def main():
     # NOTE: when passing back to the application runtime, the poscar data will be converted to a JS class instance
     # of a Material class (https://github.com/Exabyte-io/made.js/blob/dev/src/material.js).
 
-    output_materials = globals()["materials_out"] = [{"poscar": poscar}]
+    output_materials = [{"poscar": poscar}]
     print("Output materials: ", json.dumps(output_materials, indent=4))
 
     # Return the globals() dictionary to the application environment. Required for proper operation at this time.
+    globals()["materials_out"] = output_materials
     return globals()
 
 

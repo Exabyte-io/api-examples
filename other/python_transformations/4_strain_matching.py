1c25d7e08a77170f99976d6f00af186bfe9a5057
VsevolodX <79542055+VsevolodX@users.noreply.github.com>
2024-01-16 11:22:27 -0800

    chore: add imports to the code

--- other/python_transformations/4_strain_matching.py
+++ other/python_transformations/4_strain_matching.py
@@ -1,12 +1,47 @@
 """TITLE: Strain Matching of a 2D Material Layer on a Surface."""
 
 """BLOCK: Install Dependencies"""
+
+"""
+This scripts installs the required packages for a basic Pyodide environment for Materials structure manipulation.
+It uses micropip - a Python package manager for Pyodide.
+"""
 import micropip
 
-await micropip.install("uncertainties")
-print("Installed uncertainties")
-await micropip.install("https://files.mat3ra.com:44318/uploads/pymatgen-2023.9.10-py3-none-any.whl", deps=False)
-print("Installed pymatgen")
+packages = [
+    "https://files.mat3ra.com:44318/uploads/pymatgen-2023.9.10-py3-none-any.whl",
+    "https://files.mat3ra.com:44318/web/pyodide/spglib-2.0.2-py3-none-any.whl",
+    "https://files.pythonhosted.org/packages/d9/0e/2a05efa11ea33513fbdf4a2e2576fe94fd8fa5ad226dbb9c660886390974/ruamel.yaml-0.17.32-py3-none-any.whl",
+    "ase==3.22.1",
+    "networkx==3.2.1",
+    "monty==2023.11.3",
+    "scipy==1.11.1",
+    "lzma",
+    "tabulate==0.9.0",
+    "sqlite3",
+    "sympy==1.12",
+]
+
+
+async def install_package(pkg):
+    """
+    Installs a package in a Pyodide environment.
+    Args:
+        pkg: The name of the package to install.
+
+    Returns:
+        None
+    """
+    is_url = pkg.startswith("http://") or pkg.startswith("https://")
+    are_dependencies_installed = not is_url
+    await micropip.install(pkg, deps=are_dependencies_installed)
+    # Extract package name for printing
+    pkg_name = pkg.split("/")[-1].split("-")[0] if is_url else pkg.split("==")[0]
+    print(f"Installed {pkg_name}")
+
+
+for package in packages:
+    await install_package(package)
 
 """BLOCK: Utils, Class Definitions, and main()"""
 
@@ -16,6 +51,9 @@ from __future__ import annotations
 # Select materials and surface plane
 SUBSTRATE_INDEX = 0
 LAYER_INDEX = 1
+
+# Select interface from list of generated interfaces sorted by increasing mean absolute strain
+RESULT_INDEX = 0
 
 SUBSTRATE_MILLER = (1, 1, 1)
 SUBSTRATE_THICKNESS = 3
@@ -428,7 +466,7 @@ def main():
 
     plt.show()
 
-    best_interface = sorted_interfaces[0]["interface"]
+    best_interface = sorted_interfaces[RESULT_INDEX]["interface"]
     wrapped_structure = best_interface.copy()
     wrapped_structure.make_supercell([1, 1, 1])  # This should wrap the atoms inside the unit cell
     interface_poscar = wrapped_structure.to(fmt="poscar")

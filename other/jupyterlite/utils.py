3e3ddb88f4dd1ca3045254a39ffcc7e9f1b1d5ea
VsevolodX <79542055+VsevolodX@users.noreply.github.com>
2024-02-05 10:01:35 -0800

    chore: simplify utils folder structure

--- other/jupyterlite/utils.py
+++ other/jupyterlite/utils.py
@@ -1,3 +1,7 @@
+from IPython.display import display, Javascript
+import json
+import time
+
 """
 This module contains a function to install packages in a Pyodide environment.
 Pyodide uses micropip as replacement for pip to install packages.
@@ -49,3 +53,44 @@ async def install_packages(notebook_name, requirements_path="requirements.yml", 
         await install_package(package)
     if verbose:
         print("All packages installed.")
+
+
+def set_data(key, value):
+    """
+    This function takes a Python object, serializes it to JSON, and sends it to the host environment
+    through a JavaScript function defined in the JupyterLite extension `data_bridge`.
+
+    Args:
+        materials (object): The Python object to be sent to the host environment.
+    """
+    serialized_data = json.dumps({key: value})
+    js_code = f"""
+    (function() {{
+        window.sendDataToHost({serialized_data})
+        console.log({serialized_data})
+    }})();
+    """
+
+    display(Javascript(js_code))
+    print(f"Status: {key} sent to host.")
+
+
+def get_data(key):
+    """
+    This function requests materials from the host environment through a JavaScript function defined in the JupyterLite
+    extension `data_bridge`. The materials are then returned to the Python environment.
+    """
+    js_code = """
+    (function() {
+        if (window.requestDataFromHost) {
+            window.requestDataFromHost();
+            
+        } else {
+            console.error('requestDataFromHost function is not defined on the window object.');
+        }
+    })();
+    """
+
+    display(Javascript(js_code))
+    time.sleep(2)  # JS postMessage is asynchronous, so we need to wait for the response from JS host
+    print(f"Status: {key} received")

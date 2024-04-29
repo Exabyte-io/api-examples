73f94d8243a60c3af857414f621e6968b6c89f3b
VsevolodX <79542055+VsevolodX@users.noreply.github.com>
2024-04-28 20:44:33 -0700

    update: adjust plot

--- utils/plot.py
+++ utils/plot.py
@@ -1,50 +1,33 @@
-from collections import defaultdict
-
 import plotly.graph_objs as go
 
 
-def plot_strain_vs_atoms(sorted_interfaces, settings):
-    terminations = sorted_interfaces.terminations
-    # Create a mapping from termination to its index
-    termination_to_index = {termination: i for i, termination in enumerate(terminations)}
-
-    grouped_interfaces = defaultdict(list)
-    for termination, interfaces in sorted_interfaces.items():
-        for index, interface_data in enumerate(interfaces):
-            strain_percentage = interface_data["mean_abs_strain"] * 100
-            num_sites = interface_data["interface"].num_sites
-            key = (strain_percentage, num_sites)
-            grouped_interfaces[key].append((index, termination))
+def plot_strain_vs_atoms(interface_data_holder, settings):
+    sorted_interfaces = interface_data_holder.data
 
     data = []
-    for (strain, num_sites), indices_and_terminations in grouped_interfaces.items():
-        termination_indices = defaultdict(list)
-        for index, termination in indices_and_terminations:
-            termination_indices[termination].append(index)
-        all_indices = [index for indices in termination_indices.values() for index in indices]
-        index_range = f"{min(all_indices)}-{max(all_indices)}" if len(all_indices) > 1 else str(min(all_indices))
+    for termination, interfaces in sorted_interfaces.items():
+        for index, interface in enumerate(interfaces):
+            strain_percentage = interface.get_mean_abs_strain() * 100
+            num_sites = interface.num_sites
 
-        hover_text = "<br>-----<br>".join(
-            f"Termination: {termination}<br>Termination index: {termination_to_index[termination]}"
-            f"<br>Interfaces Index Range: {index_range}<br>Strain: {strain:.2f}%<br>Atoms: {num_sites}"
-            for termination, indices in termination_indices.items()
-        )
-        trace = go.Scatter(
-            x=[strain],
-            y=[num_sites],
-            text=[hover_text],
-            mode="markers",
-            hoverinfo="text",
-            name=f"Indices: {index_range}",
-        )
-        data.append(trace)
+            hover_text = f"Index: {index}<br>Strain: {strain_percentage:.2f}%<br>Atoms: {num_sites}"
+
+            trace = go.Scatter(
+                x=[strain_percentage],
+                y=[num_sites],`
+                text=[hover_text],
+                mode="markers",
+                hoverinfo="text",
+                name=f"Index: {index}",
+            )
+            data.append(trace)
 
     layout = go.Layout(
-        xaxis=dict(title="Strain (%)", type=settings["X_SCALE"]),
-        yaxis=dict(title="Number of atoms", type=settings["Y_SCALE"]),
-        hovermode="closest",
-        height=settings["HEIGHT"],
-        legend_title_text="Interfaces Index Range",
+    xaxis=dict(title="Strain (%)", type=settings["X_SCALE"]),
+    yaxis=dict(title="Number of atoms", type=settings["Y_SCALE"]),
+    hovermode="closest",
+    height=settings["HEIGHT"],
+    legend_title_text="Interfaces Index Range",
     )
     fig = go.Figure(data=data, layout=layout)
     fig.show()

61143507e392fb1b621c240dbebb5dd01e76d262
VsevolodX <79542055+VsevolodX@users.noreply.github.com>
2024-05-03 15:14:42 -0700

    update: fix plotting

--- utils/plot.py
+++ utils/plot.py
@@ -1,7 +1,7 @@
 from typing import Dict, Union
 
 import plotly.graph_objs as go
-from mat3ra.made.tools.build.interface import InterfaceDataHolder
+from mat3ra.made.tools.build.interface import InterfaceDataHolder, StrainModes
 
 
 def plot_strain_vs_atoms(interface_data_holder: InterfaceDataHolder, settings: Dict[str, Union[str, int]]):
@@ -12,32 +12,31 @@ def plot_strain_vs_atoms(interface_data_holder: InterfaceDataHolder, settings: D
         interface_data_holder (InterfaceDataHolder): The interface data holder object.
         settings (Dict[str, Union[str, int]]): The settings for the plot.
     """
-    sorted_interfaces = interface_data_holder.interfaces
+    sorted_interfaces = interface_data_holder.get_interfaces_for_termination(0)
 
     data = []
-    for termination, interfaces in sorted_interfaces.items():
-        for index, interface in enumerate(interfaces):
-            strain_percentage = interface.get_mean_abs_strain() * 100
-            num_sites = interface.num_sites
+    for index, interface in enumerate(sorted_interfaces):
+        strain_percentage = interface.interface_properties[StrainModes.mean_abs_strain]
+        num_sites = interface.num_sites
 
-            hover_text = f"Index: {index}<br>Strain: {strain_percentage:.2f}%<br>Atoms: {num_sites}"
+        hover_text = f"Index: {index}<br>Strain: {strain_percentage:.2f}%<br>Atoms: {num_sites}"
 
-            trace = go.Scatter(
-                x=[strain_percentage],
-                y=[num_sites],
-                text=[hover_text],
-                mode="markers",
-                hoverinfo="text",
-                name=f"Index: {index}",
-            )
-            data.append(trace)
+        trace = go.Scatter(
+            x=[strain_percentage],
+            y=[num_sites],
+            text=[hover_text],
+            mode="markers",
+            hoverinfo="text",
+            name=f"Index: {index}",
+        )
+        data.append(trace)
 
     layout = go.Layout(
         xaxis=dict(title="Strain (%)", type=settings["X_SCALE"]),
         yaxis=dict(title="Number of atoms", type=settings["Y_SCALE"]),
         hovermode="closest",
         height=settings["HEIGHT"],
-        legend_title_text="Interfaces Index Range",
+        legend_title_text="Interfaces Indices",
     )
 
     fig = go.Figure(data=data, layout=layout)

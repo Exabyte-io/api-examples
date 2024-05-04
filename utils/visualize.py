a54a1a0b23fae2082dacc9f0d6de3b1c73df202f
VsevolodX <79542055+VsevolodX@users.noreply.github.com>
2024-05-03 19:52:03 -0700

    update: use grid to display

--- utils/visualize.py
+++ utils/visualize.py
@@ -1,10 +1,10 @@
-import base64
 import io
 from typing import List, Union
 
+import ipywidgets as widgets
 from ase.build import make_supercell
 from ase.io import write
-from IPython.display import HTML, display
+from IPython.display import display
 from mat3ra.made.material import Material
 from mat3ra.made.tools.convert import to_ase
 from mat3ra.utils.array import convert_to_array_if_not
@@ -14,8 +14,16 @@ def get_material_visualization_html(
     material: Material, title: str, rotation: str = "0x", number_of_repetitions: int = 1
 ):
     """
-    Returns an HTML string with a Base64-encoded image for visualization,
-    including the name of the file, positioned horizontally.
+    Returns an image of the material structure with the specified title.
+
+    Args:
+        material (Material): Material object to visualize.
+        title (str): Title of the image.
+        rotation (str): Rotation of the image.
+        number_of_repetitions (int): Number of repetitions of the unit cell.
+
+    Returns:
+        tuple: Tuple containing the image and the title.
     """
 
     material = to_ase(material)
@@ -28,20 +36,45 @@ def get_material_visualization_html(
     buf = io.BytesIO()
     write(buf, material_repeat, format="png", rotation=rotation)
     buf.seek(0)
-    img_str = base64.b64encode(buf.read()).decode("utf-8")
-    html_str = f"""
-    <div style="display: inline-block; margin: 10px; vertical-align: top;">
-        <p>{text}</p>
-        <img src="data:image/png;base64,{img_str}" alt="{title}" />
-    </div>
+    return (buf.read(), text)
+
+
+def create_responsive_image_grid(image_tuples, max_columns=3):
     """
-    return html_str
+    Create a responsive image grid that can display images from a specified folder.
+
+    Args:
+        image_tuples (list): List of tuples where each tuple contains an image and a title.
+        max_columns (int): Maximum number of columns in the grid.
+    """
+    items = [
+        widgets.VBox(
+            [
+                widgets.Label(value=title, layout=widgets.Layout(height="30px", overflow="hidden")),
+                widgets.Image(value=image, format="png", width="auto", height="auto"),
+            ]
+        )
+        for image, title in image_tuples
+    ]
+
+    column_width = f"minmax(100px, {100 / max_columns}%)"
+
+    grid = widgets.GridBox(
+        items,
+        layout=widgets.Layout(
+            grid_template_columns=f"repeat({max_columns}, {column_width})",
+            grid_gap="10px",
+            width="100%",
+        ),
+    )
+    return grid
 
 
 def visualize(materials: Union[List[Material], Material], title: str = "Material", number_of_repetitions: int = 1):
     materials = convert_to_array_if_not(materials)
-    for i, material in enumerate(materials):
-        material_image = get_material_visualization_html(
-            material, title=f"{title} {i}", number_of_repetitions=number_of_repetitions
-        )
-        display(HTML(material_image))
+    items = [
+        get_material_visualization_html(material, title=f"{title} {i}", number_of_repetitions=number_of_repetitions)
+        for i, material in enumerate(materials)
+    ]
+
+    display(create_responsive_image_grid(items))

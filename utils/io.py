33777c2dc8754491f199521ac5a00dac0eda0535
VsevolodX <79542055+VsevolodX@users.noreply.github.com>
2024-05-07 21:37:20 -0700

    update: sync and async functionality

--- utils/io.py
+++ utils/io.py
@@ -1,24 +1,44 @@
-from typing import Any, List, Optional
+from typing import Any, Awaitable, Callable, List, Optional
 
 
-def user_select_object_by_index(
-    objects: List[Any], object_name: str = "object", prompt_head: Optional[str] = None
+def generate_prompt_text(array: List[Any]) -> str:
+    return "\n".join(f"{i}: {t}" for i, t in enumerate(array))
+
+
+async def async_input(prompt: str) -> str:
+    return input(prompt)
+
+
+def select_index(
+    input_func: Callable[[str], Awaitable[str] or str],
+    array: List[Any],
+    element_name: str,
+    prompt_head: Optional[str] = None,
 ) -> Any:
-    prompt_head = prompt_head or f"Select {object_name} by index:\n"
-    prompt_body = "\n".join(f"{i}: {t}" for i, t in enumerate(objects))
-
-    selected_index_str = input(prompt_head + prompt_body)
+    prompt_head = prompt_head or f"Select {element_name} by index:\n"
+    prompt_body = generate_prompt_text(array)
+    selected_index_str = input_func(prompt_head + prompt_body)
     try:
         selected_index = int(selected_index_str)
     except ValueError:
         print("Invalid input. Please enter a valid integer.")
-        return user_select_object_by_index(objects, object_name, prompt_head)
+        return select_index(input_func, array, element_name, prompt_head)
 
-    assert isinstance(selected_index, int)
-
-    if selected_index < 0 or selected_index >= len(objects):
+    if selected_index < 0 or selected_index >= len(array):
         print("Invalid index.")
-        return user_select_object_by_index(objects, object_name, prompt_head)
+        return select_index(input_func, array, element_name, prompt_head)
 
-    print(f"Selected {object_name}: ", objects[selected_index])
-    return objects[selected_index]
+    print(f"Selected {element_name}: ", array[selected_index])
+    return array[selected_index]
+
+
+def user_select_array_element_by_index(
+    array: List[Any], element_name: str = "element", prompt_head: Optional[str] = None
+) -> Any:
+    return select_index(input, array, element_name, prompt_head)
+
+
+async def user_select_array_element_by_index_async(
+    array: List[Any], element_name: str = "element", prompt_head: Optional[str] = None
+) -> Any:
+    return await select_index(async_input, array, element_name, prompt_head)

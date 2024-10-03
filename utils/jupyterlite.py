5db5512b394c44366453a26b465b3bbec313b051
VsevolodX <79542055+VsevolodX@users.noreply.github.com>
2024-10-02 19:26:59 -0700

    update: read verbose flag from envrion

--- utils/jupyterlite.py
+++ utils/jupyterlite.py
@@ -33,6 +33,35 @@ if ENVIRONMENT == EnvironmentEnum.PYTHON:
     import sys
 
 
+def log(message: str, level: Optional[SeverityLevelEnum] = None, force_verbose=None):
+    """
+    Log a message based on the VERBOSE flag in the caller's globals().
+
+    Args:
+        message (str): The message to log.
+        level (SeverityLevelEnum): The severity level of the message (e.g., INFO, WARNING, ERROR).
+        force_verbose (bool): If True, log the message regardless of the VERBOSE flag in globals()
+    """
+    if force_verbose is True:
+        should_log = True
+    elif force_verbose is False:
+        should_log = False
+    else:
+        # Inspect the caller's globals to get VERBOSE flag
+        frame = inspect.currentframe()
+        try:
+            caller_frame = frame.f_back  # type: ignore
+            caller_globals = caller_frame.f_globals  # type: ignore
+            should_log = caller_globals.get("VERBOSE", os.environ.get("VERBOSE", True))
+        finally:
+            del frame  # Avoid reference cycles
+    if should_log:
+        if level is None:
+            print(message)
+        else:
+            print(f"{level.value}: {message}")
+
+
 async def install_package_pyodide(pkg: str, verbose=True):
     """
     Install a package in a Pyodide environment.
@@ -264,32 +293,3 @@ def set_materials(materials: List[Material]):
     """
     materials_data = [material.to_json() for material in materials]
     set_data("materials", materials_data)
-
-
-def log(message: str, level: Optional[SeverityLevelEnum] = None, force_verbose=None):
-    """
-    Log a message based on the VERBOSE flag in the caller's globals().
-
-    Args:
-        message (str): The message to log.
-        level (SeverityLevelEnum): The severity level of the message (e.g., INFO, WARNING, ERROR).
-        force_verbose (bool): If True, log the message regardless of the VERBOSE flag in globals()
-    """
-    if force_verbose is True:
-        should_log = True
-    elif force_verbose is False:
-        should_log = False
-    else:
-        # Inspect the caller's globals to get VERBOSE flag
-        frame = inspect.currentframe()
-        try:
-            caller_frame = frame.f_back  # type: ignore
-            caller_globals = caller_frame.f_globals  # type: ignore
-            should_log = caller_globals.get("VERBOSE", False)
-        finally:
-            del frame  # Avoid reference cycles
-    if should_log:
-        if level is None:
-            print(message)
-        else:
-            print(f"{level.value}: {message}")

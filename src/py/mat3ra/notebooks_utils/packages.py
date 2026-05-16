4e86633a3d914cf69451f71b49d3242d075a612a
VsevolodX <vsevolodbiryukov@icloud.com>
2026-05-15 20:54:25 -0700

    update: adjust installation

--- src/py/mat3ra/notebooks_utils/packages.py
+++ src/py/mat3ra/notebooks_utils/packages.py
@@ -1,47 +1,6 @@
-import importlib
-import json
-import os
-from typing import List
-
-from .ipython.packages.install import get_packages_list, install_package_python
-from .primitive.enums import EnvironmentsEnum
-from .primitive.environment import ENVIRONMENT, is_pyodide_environment
-from .primitive.logger import log
-from .pyodide.packages.install import get_config_yml_file_path, install_init, install_package_pyodide
-
-
-async def install_package(pkg: str, verbose: bool = True):
-    """
-    Install a package in the current environment.
-
-    Args:
-        pkg (str): The name of the package to install.
-        verbose (bool): Whether to print the name of the installed package.
-    """
-    if ENVIRONMENT == EnvironmentsEnum.PYODIDE:
-        await install_package_pyodide(pkg, verbose)
-    elif ENVIRONMENT == EnvironmentsEnum.PYTHON:
-        install_package_python(pkg, verbose)
-
-
-async def install_packages_with_hashing(packages: List[str], verbose: bool = True):
-    """
-    Install the given packages, skipping if the list is unchanged since last run.
-
-    Args:
-        packages (List[str]): The list of packages to install.
-        verbose (bool): Whether to print the names of the installed packages.
-    """
-    requirements_hash = str(hash(json.dumps(packages)))
-    if os.environ.get("requirements_hash") != requirements_hash:
-        for pkg in packages:
-            await install_package(pkg, verbose)
-        if verbose:
-            log("Packages installed successfully.", force_verbose=verbose)
-        os.environ["requirements_hash"] = requirements_hash
-    else:
-        if verbose:
-            log("Packages are already installed.", force_verbose=verbose)
+from .ipython.packages.install import install_packages_python
+from .primitive.environment import is_pyodide_environment
+from .pyodide.packages.install import install_packages_pyodide
 
 
 async def install_packages(notebook_name_pattern: str, config_file_path: str = "", verbose: bool = True):
@@ -58,11 +17,6 @@ async def install_packages(notebook_name_pattern: str, config_file_path: str = "
         verbose (bool): Whether to print install progress.
     """
     if is_pyodide_environment():
-        await install_init()
-
-    yaml = importlib.import_module("yaml")
-    with open(get_config_yml_file_path(config_file_path), "r") as f:
-        requirements_dict = yaml.safe_load(f)
-
-    packages = get_packages_list(requirements_dict, notebook_name_pattern)
-    await install_packages_with_hashing(packages, verbose)
+        await install_packages_pyodide(notebook_name_pattern, verbose)
+    else:
+        install_packages_python(notebook_name_pattern, verbose)
